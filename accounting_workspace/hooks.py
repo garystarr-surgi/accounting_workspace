d83dd78d16a53b1981c34ee5574b9878a5ae460c
garystarr-surgi <gary.starr@surgishop.com>
2026-02-23 11:50:19 -0500

    Create hooks.py

---
+++ accounting_workspace/hooks.py
@@ -0,0 +1,10 @@
+app_name = "accounting_workspace"
+app_title = "Accounting Workspace"
+app_publisher = "SurgiShop"
+app_description = "Create an Accounting Workspace"
+app_email = "gary.starr@surgishop.com"
+app_license = "MIT"
+required_apps = ["frappe/erpnext"]
+required_frappe_version = ">=16.0.0"
+
+
